1ded257a19d93389b64fb3fcfcf44bc413236b49
Oliv <oallardjacquin@gmail.com>
2019-11-27 15:00:55 +0100

    save

--- src/App.js
+++ src/App.js
@@ -11,55 +11,61 @@ import Langues from './components/Langues';
 import Liens from './components/Liens';
 import Profil from './components/Profil';
 import Meteo from './components/Meteo';
+import Portfolio from './components/Portfolio';
 
 function App() {
 
   return (
 
-<div className={"App"}>
-    <Container>
-      <Row>
-        <Col><NavigationBar /></Col>
-      </Row>
-      <Row>
+    <div className={"App"}>
+      <Container>
+        <Row>
+          <Col><NavigationBar /></Col>
+        </Row>
+        <Row>
 
-      </Row>
-      <Row>
-        <Col xs="12" md="4">
-          <Row>
-            <Col >
-              <Contacts />
-            </Col>
-          </Row>
+        </Row>
+        <Row>
+          <Col xs="12" md="4">
             <Row>
-                <Col>
-                    <Liens />
-                </Col>
+              <Col >
+                <Contacts />
+              </Col>
             </Row>
-          <Row>
-            <Col>
-              <Competences />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs="12" md="8">
-          <Profil />
-          <Experiences />
-          <Row>
-            <Col xs="12" md="8">
-              <Formation />
-            </Col>
-            <Col xs="12" md="4">
-              <Langues />
-              <Meteo />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+            <Row>
+              <Col>
+                <Liens />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Competences />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Portfolio />
+              </Col>
+            </Row>
+          </Col>
+          <Col xs="12" md="8">
+            <Profil />
+            <Experiences />
+            <Row>
+              <Col xs="12" md="8">
+                <Formation />
+              </Col>
+              <Col xs="12" md="4">
+                <Langues />
+                <Meteo />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
 
-      <br /> <br /> <br />
-    </Container>
-</div>
+        <br /> <br /> <br />
+      </Container>
+    </div>
 
 
   );
